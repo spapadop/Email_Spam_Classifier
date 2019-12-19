1add62a5d0057a7a1d45696fa02dd3fc41c083db
spapadop <sokratis.papadop@gmail.com>
2019-12-19 23:58:58 +0100

    working pipeline - 97% accuracy with SVM

--- src/main/scala/spam.scala
+++ src/main/scala/spam.scala
@@ -1,23 +1,25 @@
-/*
-Acknowledgement: the implementation is based on Spark native tutorial for ML:
-- https://spark.apache.org/docs/2.2.0/ml-features.html
-- https://spark.apache.org/docs/2.2.0/ml-classification-regression.html#linear-support-vector-machine
+/**
+ * Acknowledgement: the implementation is based on Spark native tutorial for ML:
+ * --- https://spark.apache.org/docs/2.2.0/ml-features.html
+ * --- https://spark.apache.org/docs/2.2.0/ml-classification-regression.html#linear-support-vector-machine
+ * --- https://spark.apache.org/docs/latest/ml-tuning.html
  */
 
-import org.apache.spark.ml.classification.{DecisionTreeClassifier, LinearSVC, LogisticRegression, NaiveBayes, RandomForestClassifier}
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
-import org.apache.spark.ml.feature.{CountVectorizer, HashingTF, IDF, NGram, StopWordsRemover, Tokenizer, VectorAssembler}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{DecisionTreeClassifier, LinearSVC, LogisticRegression, NaiveBayes}
+import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
+import org.apache.spark.ml.feature.{HashingTF, IDF, StopWordsRemover, Tokenizer, VectorAssembler}
+import org.apache.spark.ml.tuning.{CrossValidator, ParamGridBuilder}
 import org.apache.spark.mllib.feature.Stemmer
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.ml.linalg.Vector
+import java.util.regex.Pattern
 
 object spam {
 
-
   def main(args: Array[String]): Unit = {
 
-    // start spark session
     val spark = SparkSession.builder.master("local").appName("Classification").getOrCreate()
-
     import spark.implicits._
 
     // =======================================================
@@ -50,20 +52,35 @@ object spam {
     val testing_df = testing_dataset.toDF("sentences", "label", "dataset")
 
     // =======================================================
-    // Feature representation: Bag of Words
+    // Feature representation: Bag of Words, URLs
     // =======================================================
 
+    import org.apache.spark.sql.functions._
+
+    // set a UDF to check whether a sentence contains a url
+    val isUrl: String => Boolean = { t => t.contains("www.") || t.contains("http") }
+    val isUrlUDF = udf(isUrl)
+
+    val training_url = training_df.withColumn("url", isUrlUDF(training_df.col("sentences")))
+    val testing_url = testing_df.withColumn("url", isUrlUDF(testing_df.col("sentences")))
+
+    // set a UDF to check whether a sentence contains phone number
+//    val regexStr: String = "^[0-9]{10}$"
+//    val pattern = Pattern.compile(regexStr)
+//    val hasNumber: String => Boolean = { t => { val matcher = pattern.matcher(t.toString)
+//                                                return matcher.matches()}
+//                                        }
+//    val isNumber = udf(hasNumber)
+//
+//    val training_number = training_url.withColumn("number", isNumber(training_df.col("sentences")))
+//    val testing_number = testing_url.withColumn("number", isNumber(testing_df.col("sentences")))
+
     val tokenizer = new Tokenizer().setInputCol("sentences").setOutputCol("words")
-    val training_words = tokenizer.transform(training_df) // tokenize the training set into words
-    val testing_words = tokenizer.transform(testing_df) // tokenize the testing set into words
 
     // stopwords elimination
     val remover = new StopWordsRemover()
       .setInputCol("words")
       .setOutputCol("words_noStopWords")
-
-    val training_words_noStopWords = remover.transform(training_words) //remove stopwords from training set
-    val testing_words_noStopWords = remover.transform(testing_words) //remove stopwords from test set
 
     // stemming
     val stemmer = new Stemmer()
@@ -71,27 +88,18 @@ object spam {
       .setOutputCol("words_filtered")
       .setLanguage("English")
 
-    val training_words_filtered = stemmer.transform(training_words_noStopWords)
-    val testing_words_filtered = stemmer.transform(testing_words_noStopWords)
-
-    // applying Hashing TF to convert input words into feature vectors
-    val hashingTF = new HashingTF().setInputCol("words_filtered").setOutputCol("rawFeatures").setNumFeatures(20)
-
     // =======================================================
     // Feature weighting: TF-IDF
     // =======================================================
 
-    val training_featured_data = hashingTF.transform(training_words_filtered) // calculate TF for training set words
-    val testing_featured_data = hashingTF.transform(testing_words_filtered) // calculate TF for testing set words
+    // applying Hashing TF to convert input words into feature vectors
+    val hashingTF = new HashingTF().setInputCol("words_filtered").setOutputCol("rawFeatures").setNumFeatures(2000)
 
-    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    val training_IDF_model = idf.fit(training_featured_data) // calculate IDF for training featured data
-    val testing_IDF_model = idf.fit(testing_featured_data) // calculate IDF for testing featured data
+    val idf = new IDF().setInputCol("rawFeatures").setOutputCol("idf_features")
 
-    val training_data = training_IDF_model.transform(training_featured_data)
-    val testing_data = testing_IDF_model.transform(testing_featured_data)
-    //    training_data.select("label", "features").show()
-    //    testing_data.select("label", "features").show()
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("idf_features", "url"))
+      .setOutputCol("features")
 
     // =======================================================
     // Classification methods: Naive Bayes, SVM, Decision Tree
@@ -100,60 +108,65 @@ object spam {
     // =======================================================
 
     // - Naive Bayes
-    val bayes_model = new NaiveBayes().fit(training_data)
-    val bayes_predictions = bayes_model.transform(testing_data)
+    val bayes = new NaiveBayes()
+    //    val bayes_model = new NaiveBayes().fit(training_data)
+    //    val bayes_predictions = bayes_model.transform(testing_data)
 
     // - SVM
-    val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
-    val svm_model = lsvc.fit(training_data)
-    val svm_predictions = svm_model.transform(testing_data)
+    val lsvc = new LinearSVC()
+    val svm = lsvc.setMaxIter(10).setRegParam(0.1)
 
     // - Logistic Regression
-    //    val mlr = new LogisticRegression()
-    //      .setMaxIter(10)
-    //      .setRegParam(0.3)
-    //      .setElasticNetParam(0.8)
-    //      .setFamily("multinomial")
-    //
-    //    val mlrModel = mlr.fit(training_data)
-    //    val lr_predictions = mlrModel.transform(testing_data)
+    val lr = new LogisticRegression()
+//    val lr_model = lr
+//      .setMaxIter(10)
+//      .setRegParam(0.3)
+//      .setElasticNetParam(0.8)
+//      .setFamily("multinomial")
 
     // - Decision tree
     val dt = new DecisionTreeClassifier()
-      .setLabelCol("label")
-      .setFeaturesCol("features")
-    //      .setNumTrees(10)
+//    val dt_model = dt
+//      .setLabelCol("label")
+//      .setFeaturesCol("features")
 
-    val dt_model = dt.fit(training_data)
-    val dt_predictions = dt_model.transform(testing_data)
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, remover, stemmer, hashingTF, idf, assembler, lsvc))
+
+    //    val model = pipeline.fit(training_url)
+    //    val test = model.transform(testing_url)
 
     // =======================================================
-    // Classifier Evaluation:
-    // - Precision
-    // - Recall
-    // - Accuracy
-    // - F1-score
+    // Cross validation
     // =======================================================
 
-    printMetric("accuracy", bayes_predictions, "Bayes")
-    printMetric("weightedPrecision", bayes_predictions, "Bayes")
-    printMetric("weightedRecall", bayes_predictions, "Bayes")
-    printMetric("f1", bayes_predictions, "Bayes")
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(hashingTF.numFeatures, Array(2000, 5000))
+      .addGrid(lsvc.maxIter, Array(1000))
+      .build()
 
-    printMetric("accuracy", svm_predictions, "SVM")
-    printMetric("weightedPrecision", svm_predictions, "SVM")
-    printMetric("weightedRecall", svm_predictions, "SVM")
-    printMetric("f1", svm_predictions, "SVM")
+    val cv = new CrossValidator()
+      .setEstimator(pipeline)
+      .setEvaluator(new MulticlassClassificationEvaluator())
+      .setEstimatorParamMaps(paramGrid)
+      .setNumFolds(2)
 
-    printMetric("accuracy", dt_predictions, "Random Forest")
-    printMetric("weightedPrecision", dt_predictions, "Random Forest")
-    printMetric("weightedRecall", dt_predictions, "Random Forest")
-    printMetric("f1", dt_predictions, "Random Forest")
+    // Run cross-validation, and choose the best set of parameters.
+    val cvModel = cv.fit(training_url)
+    val result = cvModel.transform(testing_url)
 
-    //    printMetric("accuracy",lr_predictions, "Linear Regression")
-    //    printMetric("weightedPrecision",lr_predictions, "Linear Regression")
-    //    printMetric("weightedRecall",lr_predictions, "Linear Regression")
-    //    printMetric("f1",lr_predictions, "Linear Regression")
+    // print metrics
+    printMetric("accuracy", result, "SVM")
+    printMetric("f1", result, "SVM")
+
+    // =======================================================
+    // Classifier Evaluation: Accuracy, F1-score
+    // =======================================================
+
+    //    printMetric("accuracy", test, "SVM")
+    //    printMetric("weightedPrecision", bayes_predictions, "Bayes")
+    //    printMetric("weightedRecall", bayes_predictions, "Bayes")
+    //    printMetric("f1", test, "SVM")
 
   }
 
@@ -164,30 +177,6 @@ object spam {
       .setMetricName(metric)
     val result = evaluator.evaluate(model_predictions)
     println(model + " classifier | " + metric + "=\t" + result)
-
   }
 
-  def buildNgrams(inputCol: String = "word_filtered",
-                  outputCol: String = "features", n: Int = 3) = {
-
-    val ngrams = (1 to n).map(i =>
-      new NGram().setN(i)
-        .setInputCol(inputCol).setOutputCol(s"${i}_grams")
-    )
-
-    val vectorizers = (1 to n).map(i =>
-      new CountVectorizer()
-        .setInputCol(s"${i}_grams")
-        .setOutputCol(s"${i}_counts")
-    )
-
-    val assembler = new VectorAssembler()
-      .setInputCols(vectorizers.map(_.getOutputCol).toArray)
-      .setOutputCol(outputCol)
-
-//    new Pipeline().setStages((ngrams ++ vectorizers :+ assembler).toArray)
-
-  }
-
-  val df = Seq((1, Seq("a", "b", "c", "d"))).toDF("id", "tokens")
 }
